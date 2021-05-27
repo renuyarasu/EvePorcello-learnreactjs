b74121f8a6a55c230f6ff7b66c63793aa8f0be99
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-27 22:19:41 +0530

    React.createElement: styles in index.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Learn React</h1>
+    <h1></h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement('h1', {style: {color:'crimson'}}, 'Hellow World!'),
   document.getElementById('root')
 );
 reportWebVitals();
