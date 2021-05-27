4c512e882c3216ebc04c2e7250287d36c8f13aa8
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-27 22:59:49 +0530

    Creating a React component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-//Introducing and Understand JSX
-// let city = 'Hyderabad';
-let city = {
-  name: "Hyderabad",
-  coutry: "India",
-};
+//Creating a React component;
+function Hello() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This error occurred during the build time and cannot be dismissed.</p>
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <h1>
-    {city.name} in {city.coutry}
-  </h1>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Hello />, document.getElementById("root"));
