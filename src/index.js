197d1f691702b0a0459b07442f3e43f663f9aa50
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-27 22:30:24 +0530

    React.createElement: adding  multi elements in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  React.createElement('h1', {style: {color:'crimson'}}, 'Hellow World!'),
-  document.getElementById('root')
+  // React.createElement('div', {style: {color:'crimson'}},
+  // React.createElement('h1', null, 'Hellow World!')),
+
+  React.createElement(
+    "ul",
+    {
+      style: {
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        color: "crimson",
+      },
+    },
+    React.createElement("li", null, "Apples"),
+    React.createElement("li", null, "Oranges"),
+    React.createElement("li", null, "Bannas"),
+    React.createElement("li", null, "Mangoes")
+  ),
+
+  document.getElementById("root")
 );
 reportWebVitals();
