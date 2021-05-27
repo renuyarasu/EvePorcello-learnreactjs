742af0d52c951c043f6b159e0b40daacab2bb649
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-27 22:48:17 +0530

    Understand JSX

--- src/index.js
+++ src/index.js
@@ -2,39 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
+//Introducing and Understand JSX
+// let city = 'Hyderabad';
+let city = {
+  name: "Hyderabad",
+  coutry: "India",
+};
 
 ReactDOM.render(
-  // React.createElement('div', {style: {color:'crimson'}},
-  // React.createElement('h1', null, 'Hellow World!')),
-
-  /*
-  React.createElement(
-
-    "ul",
-    {
-      style: {
-        fontWeight: "bold",
-        textTransform: "uppercase",
-        color: "crimson",
-      },
-    },
-    React.createElement("li", null, "Apples"),
-    React.createElement("li", null, "Oranges"),
-    React.createElement("li", null, "Bannas"),
-    React.createElement("li", null, "Mangoes")
-  ),
-*/
-
-  //Introducing JSX
-  <ul>
-    <li>Apples</li>
-    <li>Oranges</li>
-    <li>Bannas</li>
-    <li>Mangoes</li>
-    <li>Ghrapes</li>
-  </ul>,
-
+  <h1>
+    {city.name} in {city.coutry}
+  </h1>,
   document.getElementById("root")
 );
-reportWebVitals();
